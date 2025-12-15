0ad82e0ecae1b07cbc87d172fae577c5fb314511
Piero Abal Robles <abalrobles7749@gmail.com>
2025-12-14 20:08:41 -0500

    Other update, sorry for the conflicts with yours versions

--- Cobranza360/CobranzaWeb/obj/Debug/net10.0/CobranzaWeb.AssemblyInfo.cs
+++ Cobranza360/CobranzaWeb/obj/Debug/net10.0/CobranzaWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CobranzaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca0914d8a10eb0d21d4a24f66f8300452202aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CobranzaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CobranzaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
